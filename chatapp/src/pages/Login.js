87b9550847fe8de6dbc66b03ca547985bb5002e7
kinako825 <snowberries@gmail.com>
2025-02-24 15:03:09 +0900

    chatアプリ作成

--- chatapp/src/pages/Login.js
+++ chatapp/src/pages/Login.js
@@ -7,8 +7,16 @@ const Login = () => {
             <form>
                 <div>
                     <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" name="email" placeholder="email" />
                 </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="password" />
+                </div>
+                <button type="submit">Login</button>
             </form>
         </>
-    )
-}
+    );
+};
+
+export default Login;
