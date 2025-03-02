0eddfc2fe8aec5baf551b7c3aa3039f8a2436ea7
kinako825 <snowberries@gmail.com>
2025-03-02 16:25:15 +0900

    todoアプリ

--- todoapp/src/index.js
+++ todoapp/src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client';  // ここを変更
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));  // createRoot を使用
-root.render(<App />);  // render は createRoot で呼び出す
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);  
